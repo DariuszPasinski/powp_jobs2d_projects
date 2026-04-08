5de9e3a4388f12034f903923deedd399ef3a4891
DariuszPasiński <245897@edu.p.lodz.pl>
2026-04-08 13:21:33 +0200

    Created basis of ImmutableCompoundCommand.

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
@@ -0,0 +1,31 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ImmutableCompoundCommand implements ICompoundCommand {
+    private List<DriverCommand> commands;
+    private String name;
+
+    public ImmutableCompoundCommand(@Nullable String name, List<DriverCommand> commands) {
+        if (name == null) {
+            this.name = "ImmutableCompoundCommand";
+        } else {
+            this.name = name;
+        }
+        this.commands = commands;
+    }
+
+    @Override
+    public Iterator<DriverCommand> iterator() {
+        return null;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+
+    }
+}
